8207a4c7bc97d980793939a374e2a80529ccfcfa
Theo Carnavos <tcarnavos@gmail.com>
2016-05-19 22:26:17 -0500

    hamming completed

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,14 +4,18 @@ var Hamming = function() {};
 
 Hamming.prototype.compute = function(firstStrand, secondStrand) {
 	// length of both strings hopefully
+	if (firstStrand.length !== secondStrand.length) {
+		throw new Error('DNA strands must be of equal length.');
+	}
 	let strandLength = firstStrand.length;
 	let distance = 0;
 	// up until strandLength, determine if letter is the same. if not, increase difference
-	for (let i = 0; i < strandLength) {
+	for (let i = 0; i < strandLength; i++) {
 		if (firstStrand[i] !== secondStrand[i]) {
 			distance++;
 		}
 	}
+
 	return distance;
 };
 
