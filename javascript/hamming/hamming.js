ca7e2132e0da83925c7e6e9d47deafa5eadfa5b6
Carnavos <tcarnavos@gmail.com>
2016-05-19 20:47:40 -0500

    hamming test

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,7 +1,18 @@
+'use strict';
+
 var Hamming = function() {};
 
 Hamming.prototype.compute = function(firstStrand, secondStrand) {
-	
+	// length of both strings hopefully
+	let strandLength = firstStrand.length;
+	let distance = 0;
+	// up until strandLength, determine if letter is the same. if not, increase difference
+	for (let i = 0; i < strandLength) {
+		if (firstStrand[i] !== secondStrand[i]) {
+			distance++;
+		}
+	}
+	return distance;
 };
 
 module.exports = Hamming;
