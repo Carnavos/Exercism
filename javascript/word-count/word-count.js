81130f4f6a3ce97b0695a13564533862339b5f43
Theo Carnavos <tcarnavos@gmail.com>
2016-06-03 13:22:18 -0500

    complete word count longhand

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -5,12 +5,23 @@ let Words = function () {
 
 Words.prototype.count = function (inputString) {
 	// convert string to array of strings
-	let inputArray = inputString.split(" ");
+	let cleanedString = inputString.trim();
+	cleanedString = cleanedString.replace(/\n/, ' ').replace(/\t/, ' ').replace(/\s\s+/g, ' ');
+	// cleanedString = cleanedString.replace(/\A\s+/, '');
+	// cleanedString = cleanedString.replace(/\s+\Z/, '');
+	// let cleanedString = inputString.replace(/\\t\\n\/, ' ');
+	// let cleanedString = inputString.replace([^\\S], ' ');
+	let inputArray = cleanedString.split(" ");
 
 	// iterate through input array and add keys to new count object
 	let countObject = {};
 
-	inputArray.forEach(element => countObject[element.toLower()] = (countObject[element.toLower()]) ? countObject[element.toLower()] + 1 : 1);
+	inputArray.forEach(element => {
+		// preformat all entries as lowercase
+		let lowerElement = element.toLowerCase();
+		// if element isn't in countObject as a number, add, else increment
+		countObject[lowerElement] = !isNaN(countObject[lowerElement]) ? countObject[lowerElement] + 1 : 1;
+	});
 
 	return countObject;
 }
